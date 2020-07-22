9d2078658930d67566ae379f54ab73cebf298100
todorr993 <57106677+todorr993@users.noreply.github.com>
2020-07-22 19:04:36 +0200

    changes for user input

--- Main.java
+++ Main.java
@@ -3,9 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        new ServerTransaction("excel","C:\\Users\\todorr\\Desktop\\Backend task\\Level2\\ExternalInputExample.xlsx", "sqlite", "C:\\Users\\todorr\\Desktop\\Backend task\\Level2\\OutputExample2.sqlite3", 5000).go();
+        String inputFile;
+        String outputFile;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please insert input path file");
+        inputFile=scanner.nextLine();
+        //"C:\\Users\\todorr\\Desktop\\Backend task\\Level2\\ExternalInputExample.xlsx"
+        
+        System.out.println("Please insert output path file");
+        outputFile=scanner.nextLine();
+        
+        //"C:\\Users\\todorr\\Desktop\\Backend task\\Level2\\OutputExample2.sqlite3"
+        new ServerTransaction("excel",inputFile, "sqlite", outputFile, 5000).go();
         System.exit(0);
+        sc.close();
 
     }
 }
